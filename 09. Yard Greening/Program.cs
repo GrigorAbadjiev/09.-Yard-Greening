5d4f52d8c39d772a929aa17aa47477a2c6fbd044
User <User@DESKTOP-ESP2JT1>
2022-10-31 17:19:59 +0200

    Add project files.

---
+++ 09. Yard Greening/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _09._Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceSquareM = double.Parse(Console.ReadLine()) * 7.61;
+            double discount = priceSquareM * 0.18;
+            double finalPrice = priceSquareM - discount;
+            Console.WriteLine($"The final price is:{finalPrice} lv.");
+            Console.WriteLine($"The discount is:{discount} lv.");
+        }
+    }
+}
